23ca97bf471994dd31c23d98c36aae451bc1fc76
hkpy95 <85944806+hkpy95@users.noreply.github.com>
2021-06-15 19:30:39 +0530

    Add files via upload

---
+++ assn_1a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	/* hello world
+	 /*hi every one */ */
+
+	return 0;
+
+
+
+
+}
---
+++ assn_1b.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("enter values a b: \n");
+	/* scanf("%d%d",&a,&b);
+	sum= a + b;
+	printf("the addition is:%d \n",sum);*/
+	
+return 0;
+}	
